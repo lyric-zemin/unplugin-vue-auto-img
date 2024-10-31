a663992ba3e8994c111938315c5d694c90ac0f07
lyric <2443835100@qq.com>
2024-10-31 15:49:19 +0800

    chore: update redame

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import Vue from '@vitejs/plugin-vue'
-import Unplugin from '../src/vite'
+import Images from '../src/vite'
 
 export default defineConfig({
   plugins: [
     Vue(),
     Inspect(),
-    Unplugin(),
+    Images(),
   ],
 })
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,44 @@
 import type { FilterPattern } from '@rollup/pluginutils'
 
 export interface Options {
+  /**
+   * @default [/\.vue$/, /\.vue\?vue/, /\.vue\?v=/]
+   */
   include?: FilterPattern
+  /**
+   * @default [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/]
+   */
   exclude?: FilterPattern
 
+  /**
+   * @default 'src/assets/images'
+   */
   dirs?: string | string[]
+  /**
+   * @default ['.jpg', '.jpeg', '.png', '.gif']
+   */
   extensions?: string | string[]
+  /**
+   * @default true
+   */
   deep?: boolean
+  /**
+   * 将目录作为命名的一部分
+   *
+   * @default true
+   */
   directoryAsNamespace?: boolean
 
+  /**
+   * 生成 d.ts 文件路径
+   *
+   * @default 'src/images.d.ts'
+   */
   dts?: string
+  /**
+   * 图片命名空间
+   *
+   * @default 'I'
+   */
   ns?: string
 }
