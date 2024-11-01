c0ef0b08e486210fd3b2b2ed488cce3616c25e4f
lyric <2443835100@qq.com>
2024-11-01 13:25:27 +0800

    perf: update deps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import Vue from '@vitejs/plugin-vue'
-import Images from '../src/vite'
+import Images from 'unplugin-vue-auto-img/vite'
 
 export default defineConfig({
   plugins: [
